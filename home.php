8d7f5a9b6ef35f3f34429ab7b2ecd99830597da6
Victor Trindade <153675110+vTrindadev@users.noreply.github.com>
2025-05-05 17:29:52 -0300

    update

--- home.php
+++ home.php
@@ -88,7 +88,10 @@ if (!isset($_SESSION['acesso'])) {
     </div>
     <div class="opt-menu">
       <form action="logout.php" method="post">
-          <button type="submit" class="btn-menu-sair">Sair</button>
+        <button type="submit" class="button button--stroke" data-block="button">
+            <span class="button__flair"></span>
+            <span class="button__label">Sair</span>
+        </button>
       </form>
     </div>
 
@@ -101,5 +104,78 @@ if (!isset($_SESSION['acesso'])) {
 
     <script src="js/loader.js"></script>
     <script src="js/home.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+    <script>        class Button {
+        constructor(buttonElement) {
+            this.block = buttonElement;
+            this.init();
+            this.initEvents();
+        }
+
+        init() {
+            const el = gsap.utils.selector(this.block);
+            this.DOM = {
+            button: this.block,
+            flair: el(".button__flair")
+            };
+            this.xSet = gsap.quickSetter(this.DOM.flair, "xPercent");
+            this.ySet = gsap.quickSetter(this.DOM.flair, "yPercent");
+        }
+
+        getXY(e) {
+            const { left, top, width, height } = this.DOM.button.getBoundingClientRect();
+            const xTransformer = gsap.utils.pipe(
+            gsap.utils.mapRange(0, width, 0, 100),
+            gsap.utils.clamp(0, 100)
+            );
+            const yTransformer = gsap.utils.pipe(
+            gsap.utils.mapRange(0, height, 0, 100),
+            gsap.utils.clamp(0, 100)
+            );
+            return {
+            x: xTransformer(e.clientX - left),
+            y: yTransformer(e.clientY - top)
+            };
+        }
+
+        initEvents() {
+            this.DOM.button.addEventListener("mouseenter", (e) => {
+            const { x, y } = this.getXY(e);
+            this.xSet(x);
+            this.ySet(y);
+            gsap.to(this.DOM.flair, {
+                scale: 1,
+                duration: 0.8,
+                ease: "power2.out"
+            });
+            });
+
+            this.DOM.button.addEventListener("mouseleave", (e) => {
+            const { x, y } = this.getXY(e);
+            gsap.killTweensOf(this.DOM.flair);
+            gsap.to(this.DOM.flair, {
+                xPercent: x > 90 ? x + 20 : x < 10 ? x - 20 : x,
+                yPercent: y > 90 ? y + 20 : y < 10 ? y - 20 : y,
+                scale: 0,
+                duration: 0.5,
+                ease: "power2.out"
+            });
+            });
+
+            this.DOM.button.addEventListener("mousemove", (e) => {
+            const { x, y } = this.getXY(e);
+            gsap.to(this.DOM.flair, {
+                xPercent: x,
+                yPercent: y,
+                duration: 0.8,
+                ease: "power2"
+            });
+            });
+        }
+        }
+
+        document.querySelectorAll('[data-block="button"]').forEach((el) => new Button(el));
+</script>
+
 </body>
 </html>
